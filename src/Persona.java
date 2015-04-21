9367c631c51eeb84c3bf21baa720e37b2472b983
Lukasz Salamacha <LukaszSalamacha@gmail.com>
2015-04-21 09:33:25 +0200

    Dodanie komentarzy dokumentacyjnych

--- src/Persona.java
+++ src/Persona.java
@@ -9,18 +9,60 @@
  * @author Andrzej
  */
 public class Persona {
-    
+    /**
+     * pole przechowuje imie
+     */
     private String imie;
+   /**
+     * pole przechowuje nazwisko
+     */
     private String nazwisko;
+   /**
+     * pole przechowuje pseudonim 
+     */
     private String pseudonim;
+    /**
+     * pole przechowuje adres zamieszkania
+     */
     private String adresZamieszkania;
+    /**
+     * pole przechowuje date urodzenia
+     */
     private String dataUrodzenia;
+    /**
+     * pole przechowuje numer telefonu 1
+     */
     private String numerTelefonu1;
+    /**
+     * pole przechowuje numer telefonu 2
+     */
     private String numerTelefonu2;
+      /**
+     * pole przechowuje numer telefonu 3
+     */
     private String numerTelefonu3;
+      /**
+     * pole przechowuje numer telefonu 4
+     */
     private String numerTelefonu4;
+      /**
+     * pole przechowuje numer telefonu 5
+     */
     private String numerTelefonu5;
 
+      /**
+     *  konstruktor zawierający  pola : 
+     *  - imie
+     *  - nazwisko
+     *  - pseudonim
+     *  - adresZamieszkania
+     *  - data urodznia 
+     *  - numerTelefonu1
+     *  - numerTelefonu2
+     *  - numerTelefonu3
+     *  - numerTelefonu4
+     *  - numerTelefonu5
+     */
     public Persona(String imie, String nazwisko, String pseudonim, 
             String adresZamieszkania, String dataUrodzenia, 
             String numerTelefonu1, String numerTelefonu2, 
@@ -38,50 +80,90 @@ public class Persona {
         this.numerTelefonu5 = numerTelefonu5;
     }
 
+    /**
+     * metoda pobierająca imie 
+     * @return 
+     */
+    
     public String getImie() {
         return imie;
     }
 
+    /**
+     * metoda ustawiająca imie
+     * @param imie 
+     */
     public void setImie(String imie) {
         this.imie = imie;
     }
 
+    /**
+     * metoda pobierająca naziwsko
+     * @return 
+     */ 
     public String getNazwisko() {
         return nazwisko;
     }
-
+     /**
+      * metoda ustawiająca nazwisko
+      * @param nazwisko 
+      */
     public void setNazwisko(String nazwisko) {
         this.nazwisko = nazwisko;
     }
-
+     /**
+      * metoda pobierająca  pseudonim
+      * @return 
+      */
     public String getPseudonim() {
         return pseudonim;
     }
-
+     /** 
+      * metoda ustawiająca pseudonim 
+      * @param pseudonim 
+      */
     public void setPseudonim(String pseudonim) {
         this.pseudonim = pseudonim;
     }
-
+    /**
+     * metoda  pobierająca adres zamieszkania
+     * @return 
+     */
     public String getAdresZamieszkania() {
         return adresZamieszkania;
     }
-
+    /**
+     * metoda  ustawiająca adres zamieszkania
+     * @param adresZamieszkania 
+     */
     public void setAdresZamieszkania(String adresZamieszkania) {
         this.adresZamieszkania = adresZamieszkania;
     }
-
+    /**
+     * metoda  pobierająca date urodzenia 
+     * @return 
+     */
     public String getDataUrodzenia() {
         return dataUrodzenia;
     }
-
+    /**
+     * metoda ustawiająca date urodzenia 
+     * @param dataUrodzenia 
+     */
     public void setDataUrodzenia(String dataUrodzenia) {
         this.dataUrodzenia = dataUrodzenia;
     }
-
+     /** metoda pobierająca numer  telefonu 1
+      * 
+      * @return 
+      */
     public String getNumerTelefonu1() {
         return numerTelefonu1;
     }
-
+     /** 
+      *  metoda ustawiająca numer telefonu 1  
+      * @param numerTelefonu1 
+      */
     public void setNumerTelefonu1(String numerTelefonu1) {
         this.numerTelefonu1 = numerTelefonu1;
     }
