58d1ffff90d3fb9298f68905e390c60a6ac1228f
ArturRaczynski <artur.a.raczynski@gmail.com>
2015-04-26 19:13:00 +0200

    Dodałem metody do klasy głównej NotesGlowna kilka metod :
    Metodę na dodawanie osoby do listy - DodajPersone
    Metodę na dodawanie numeru do listy  - DodajNumer
    Metodę na wyszukanie danej osoby - WyszukajPersonę
    Metody na wyświetlanie listy - WyświetlListę
    Metody na usunięcie danego elementu listy - UsunPersone
    
    Oraz do metody main dodałem metodę wywołującą powyższe.

--- src/NotesGlowna.java
+++ src/NotesGlowna.java
@@ -1,21 +1,6 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class NotesGlowna {
-/*dodac metody pobierajace od uzytkownika dane osobowe, czy to przez SWINGa
-    i JPaneOption czy poprzez wczytywanie z terminala
-    dodac metode pobierajaca od uzytkownika wymagane dane do wyszukiwania
-    dodac metode wyswietlajaca wszystkie pozycje
-    dodac metode pobierajaca od uzytkownika index pozycji ktora chce uzynac
-    WAZNE! na razie trzeba tylko stworzyc te metody! nie trzeba pisac ich
-    kodu wewnetrzenego! zatem public static Persona dodajPersone(){}
-    public static void znajdzPersone(){};
-    public static void wyswietlListe(){};
-    publicstatic void usunPozycje(){};
-    wazne jedynie by przy wyswietlaniu wszystkich pozycji kazda zaczynala sie 
-    indexem pod ktorym sie znajduje, ale to juz przy implementacji, na razie
-    wystarczy przygotowac puste metody
-*/
-   
     /* Stałe - Po wpisaniu danej liczby mamy :    
         1 - możliwość dodania osoby do listy 
         2 - możliwość dodania numeru do listy
@@ -46,8 +31,6 @@ public class NotesGlowna {
     private static void UsuńPersone() {
 
     }
-    
-    
      public static void main(String[] args) {
 
         
